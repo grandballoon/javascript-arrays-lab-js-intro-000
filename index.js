9bc4bd6962f2d7a05bfe377790fa11c566386bd9
Luke Conley <luke.sovern.conley@gmail.com>
2017-11-08 01:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function prependKitten(name){
   return [name, ...kittens]
 }
 
-function removeLastKitten(){
-  kittens.slice (0, kittens.length - 1)
+function removeLastKitten(array){
+  array.slice (0, kittens.length - 1)
 }
