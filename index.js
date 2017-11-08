65b8f29f15a343a401442312435996cdc0e3db2d
Luke Conley <luke.sovern.conley@gmail.com>
2017-11-08 01:29:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function destructivelyPrependKitten(name){
   return kittens
 }
 
-function destructivelyRemoveLastKitten(name){
-  kittens.pop(name)
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
   return kittens
 }
 
